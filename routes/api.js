7773bf19dc2ce1b69ebf6bbaac45ff6f9d2291de
Utku <34550887+utku-guclu@users.noreply.github.com>
2023-12-28 17:09:23 +0300

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -1,27 +1,127 @@
+// routes/api.js
+
 'use strict';
+
+const mongoose = require('mongoose');
+
+// Import your Issue model and any other necessary dependencies
+const Issue = require('../models/Issue');
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((err) => {
+    console.error('Error connecting to MongoDB:', err);
+  });
 
 module.exports = function (app) {
 
   app.route('/api/issues/:project')
-  
-    .get(function (req, res){
-      let project = req.params.project;
-      
+
+    .get(async function (req, res) {
+      const project = req.params.project;
+      const filterObject = { project, ...req.query };
+
+      try {
+        const arrayOfResults = await Issue.find(filterObject);
+        return res.json(arrayOfResults);
+      } catch (error) {
+        console.error(error);
+        return res.json({ error: 'Error fetching issues' });
+      }
     })
-    
-    .post(function (req, res){
-      let project = req.params.project;
-      
+
+    .post(async function (req, res) {
+      const project = req.params.project;
+      const {
+        issue_title,
+        issue_text,
+        created_by,
+        assigned_to,
+        status_text
+      } = req.body;
+
+      if (!issue_title || !issue_text || !created_by) {
+        return res.json({ error: 'required field(s) missing' });
+      }
+
+      const newIssue = new Issue({
+        issue_title,
+        issue_text,
+        created_by,
+        assigned_to: assigned_to || '',
+        status_text: status_text || '',
+        open: true,
+        created_on: new Date(),
+        updated_on: new Date(),
+        project
+      });
+
+      try {
+        const savedIssue = await newIssue.save();
+        return res.json(savedIssue);
+      } catch (error) {
+        console.error(error);
+        return res.json({ error: 'Error saving new issue' });
+      }
     })
-    
-    .put(function (req, res){
-      let project = req.params.project;
-      
+
+    .put(async function (req, res) {
+      const project = req.params.project;
+      const { _id, ...updateObject } = req.body;
+
+      if (!_id) {
+        return res.json({ error: 'missing _id' });
+      }
+
+      if (Object.keys(updateObject).length < 1) {
+        return res.json({ error: 'no update field(s) sent', '_id': _id });
+      }
+
+      updateObject.updated_on = new Date();
+
+      try {
+        const updatedIssue = await Issue.findByIdAndUpdate(
+          _id,
+          updateObject,
+          { new: true }
+        );
+
+        if (updatedIssue) {
+          return res.json({ result: 'successfully updated', '_id': _id });
+        } else {
+          return res.json({ error: 'could not update', '_id': _id });
+        }
+      } catch (error) {
+        console.error(error);
+        return res.json({ error: 'Error updating issue', '_id': _id });
+      }
     })
-    
-    .delete(function (req, res){
-      let project = req.params.project;
-      
+
+    .delete(async function (req, res) {
+      const project = req.params.project;
+      const { _id } = req.body;
+
+      if (!_id) {
+        return res.json({ error: 'missing _id' });
+      }
+
+      try {
+        const deletedIssue = await Issue.findOneAndDelete({ _id });
+
+        if (deletedIssue) {
+          return res.json({ result: 'successfully deleted', '_id': _id });
+        } else {
+          return res.json({ error: 'could not delete', '_id': _id });
+        }
+      } catch (error) {
+        console.error(error);
+        return res.json({ error: 'could not delete', '_id': _id });
+      }
     });
-    
 };
